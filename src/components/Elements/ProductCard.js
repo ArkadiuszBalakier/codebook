21325f7cef094a6221ac71b27bdfdbabeaf4e642
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2023-03-08 10:08:45 +0100

    ProductCard

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom";
 
 export const ProductCard = ({ product }) => {
-  const { id, name, overview, price, rating, poster } = product;
+  const { id, name, overview, price, rating, poster, best_seller } = product;
 
   return (
     <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <Link to="/" className="relative">
-        <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">
-          Best Seller
-        </span>
+      <Link to={`products/${id}`} className="relative">
+        {best_seller && (
+          <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">
+            Best Seller
+          </span>
+        )}
         <img className="rounded-t-lg w-full h-64" src={poster} alt={name} />
       </Link>
       <div className="p-5">
-        <Link to="/">
+        <Link to={`products/${id}`}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {name}
           </h5>
@@ -32,7 +34,7 @@ export const ProductCard = ({ product }) => {
         <p className="flex justify-between items-center">
           <span className="text-2xl dark:text-gray-200">
             <span>$</span>
-            <span>29</span>
+            <span>{price}</span>
           </span>
           <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
             Add To Cart <i className="ml-1 bi bi-plus-lg"></i>
