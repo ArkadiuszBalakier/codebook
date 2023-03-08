e44b4c3b9b0abe1f5f47b9a445f01f7150dd9943
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2023-03-08 09:25:20 +0100

    add data json

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -1,27 +1,24 @@
 import { Link } from "react-router-dom";
 
-export const ProductCard = () => {
+export const ProductCard = ({ product }) => {
+  const { id, name, overview, price, rating, poster } = product;
+
   return (
     <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <Link to="/" className="relative">
         <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">
           Best Seller
         </span>
-        <img
-          className="rounded-t-lg w-full h-64"
-          src="/assets/images/10001.avif"
-          alt=""
-        />
+        <img className="rounded-t-lg w-full h-64" src={poster} alt={name} />
       </Link>
       <div className="p-5">
         <Link to="/">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            The Complete Guide to Backend Development
+            {name}
           </h5>
         </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error unde
-          quisquam magni vel eligendi nam.
+          {overview}
         </p>
 
         <div className="flex items-center my-2">
