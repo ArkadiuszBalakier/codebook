092054cc21a38588003a04600b1be79d0ad280b8
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2023-03-13 10:44:24 +0100

    cart UI

---
+++ src/pages/Cart/CartPage.js
@@ -0,0 +1,8 @@
+import { CartEmpty } from "./components/CartEmpty";
+import { CartList } from "./components/CartList";
+
+export const CartPage = () => {
+  const cartListLength = 0;
+
+  return <main>{cartListLength ? <CartList /> : <CartEmpty />}</main>;
+};
