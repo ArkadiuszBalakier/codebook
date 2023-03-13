9fcfea0cf79d38c7ad5a1923ecb26df735df8178
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2023-03-13 12:05:22 +0100

    remove from card button

--- src/pages/Cart/components/Checkout.js
+++ src/pages/Cart/components/Checkout.js
@@ -1,4 +1,8 @@
+import { useCart } from "../../../context";
+
 export const Checkout = ({ setCheckout }) => {
+  const { total } = useCart();
+
   return (
     <section>
       <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
@@ -99,7 +103,7 @@ export const Checkout = ({ setCheckout }) => {
                     type="number"
                     name="month"
                     id="month"
-                    className="inline-block w-20 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
+                    className="inline-block w-20 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
                     value="03"
                     disabled
                     required=""
@@ -108,7 +112,7 @@ export const Checkout = ({ setCheckout }) => {
                     type="number"
                     name="year"
                     id="year"
-                    className="inline-block w-20 ml-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
+                    className="inline-block w-20 ml-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
                     value="27"
                     disabled
                     required=""
@@ -132,7 +136,7 @@ export const Checkout = ({ setCheckout }) => {
                   />
                 </div>
                 <p className="mb-4 text-2xl font-semibold text-lime-500 text-center">
-                  $99
+                  ${total}
                 </p>
                 <button
                   type="submit"
