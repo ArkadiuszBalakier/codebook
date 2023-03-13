79657ae5121b3a85ee871c388dbbe84a20a6a6b5
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2023-03-13 11:41:34 +0100

    add cart item

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,8 +1,9 @@
 import { CartEmpty } from "./components/CartEmpty";
 import { CartList } from "./components/CartList";
+import { useCart } from "../../context";
 
 export const CartPage = () => {
-  const cartListLength = 0;
+  const { cartList } = useCart();
 
-  return <main>{cartListLength ? <CartList /> : <CartEmpty />}</main>;
+  return <main>{cartList.length ? <CartList /> : <CartEmpty />}</main>;
 };
