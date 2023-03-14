a2d25beb96a244e79d91108a35abdbb014dee1ac
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2023-03-14 14:45:59 +0100

    random change

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 import { FilterProvider, CartProvider } from "./context";
 import { ScrollToTop } from "./components";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
