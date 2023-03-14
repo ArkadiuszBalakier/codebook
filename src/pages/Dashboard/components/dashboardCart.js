04302f93ad603d9fbd061b87d5f4c7cfe1b41429
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2023-03-14 13:56:22 +0100

    netlfy

--- src/pages/Dashboard/components/dashboardCart.js
+++ src/pages/Dashboard/components/dashboardCart.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export const DashboardCart = ({ order }) => {
+export function DashboardCart({ order }) {
   return (
     <div className="max-w-4xl m-auto p-2 mb-5 border dark:border-slate-700">
       <div className="flex justify-between text-sm m-2 font-bold dark:text-slate-200">
@@ -35,4 +35,4 @@ export const DashboardCart = ({ order }) => {
       ))}
     </div>
   );
-};
+}
